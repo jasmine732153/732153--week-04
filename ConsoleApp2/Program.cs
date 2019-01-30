96525388d9e4bf87383977520ad6f78cf785ca31
visualstudio <visualstudio@122-PC09>
2019-01-30 11:20:05 -0500

    i added the countryside class and launch method to initiallise the program

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,20 +9,45 @@ namespace ConsoleApp2
     class Program
     {
         static void Main(string[] args)
-        
-          
 
-       
+
+
+
+        {
+
+        }
+
+        class village
+        {
+            public village nextvillage;
+            public village previousvillage;
+            public string villagename;
+            public bool isaAstrildeHere = false;
+        }
+        class countryside
+        {
+            village maple = new village();
+
+
+            village toronto = new village();
+
+            village ajax = new village();
+            village first;
+            village last;
+
+
+            public void launch()
             {
-
-            }
-
-            class village
-            {
-                public village nextvillage;
-                public string villagename;
-                public bool isaAstrildeHere = false;
+                maple.villagename = "maple";
+                maple.nextvillage = toronto;
+                toronto.villagename = "toronto";
+                toronto.nextvillage = ajax;
+                toronto.previousvillage = maple;
+                ajax.villagename = "ajax";
+                ajax.nextvillage = null;
+                ajax.nextvillage = toronto;
             }
         }
     }
 
+}
